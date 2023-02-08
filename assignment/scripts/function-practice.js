71a03a846c3e69f640c8c58e4d662e384519ff97
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-08 17:37:39 -0600

    done with questions

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -2,7 +2,7 @@ console.log('***** Function Practice *****')
 
 // Add the required code to complete the functions below
 // After _each_ function, use a console log to call the function
-// to test it and display the result
+// to test it and display tuhe result
 
 // 1. Function to return 'Hello World!'
 function hello() {
@@ -14,67 +14,89 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName(name1) {
+  
+  let greeting = 'Hello' + ' ' + (name1);
+  return greeting
 }
+console.log(helloName('Stacy!'));
 // Remember to call the function to test
 
-
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber, secondNumber ) {
+function addNumbers(firstNumber, secondNumber) {
+  let answer = firstNumber + secondNumber;
+  return answer;
   // return firstNumber + secondNumber;
 }
+console.log('in the addNumbers function:', addNumbers(5, 7));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree(num0, num1, num2 ){
-console.log(num0, num1, num2);
-let answer = num0 * num1 * num2;
-return answer;
+function multiplyThree(num0, num1, num2) {
+  let answer = num0 * num1 * num2;
+  return answer;
 }
-console.log(multiplyThree(1, 2, 3));
+console.log('in the multiplyThree function:', multiplyThree(1, 2, 3));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
-    return;
+function isPositive(number) {
+  let zero = 0;
+  if(number > zero) {
+  return true;
   }
-    return;
+  else {
+    return false;
+  }
+
 }
+console.log('question 5', isPositive(7));
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log('isPositive - should say true', isPositive(3));
+console.log('isPositive - should say false', isPositive(0));
+console.log('isPositive - should say false', isPositive(-3));
 
 
 // 6. Function to return the _last_ item in an array. If the 
-//    array is empty, return `undefined`.
-function getLast( array ) {
-
+function getLast(array){
+  return array[array.length - 1];
 }
+console.log("question 6", getLast([1,2,3,4,5]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
-  
-}
 
-// ----------------------
-// Stretch Goals
+function find(value, array) {
+  for ( i = 0; i < array.length; i++) {
+
+    if( value === array[i]){
+      return true;
+    }
+    return false
+}
+}
+console.log('question 7', find(9,[2,4,6]));
+
+
+
+
+/Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
-//    string. Return true if it is, and false otherwise
+// string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
 
-}
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+  }
+console.log('question 8', isFirstLetter('k','hello'));
+
+
+console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
+console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll() {
   let sum = 0
   // TODO: loop to add items
   return sum;
